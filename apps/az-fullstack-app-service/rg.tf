dcbc7d2d780f84e6b4a9e50d0661b6c496c159ce
Example <mrsauravsahu@outlook.com>
2024-06-18 02:19:56 +0530

    chore: remove other files

--- apps/az-fullstack-app-service/rg.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  resource_group_name = coalesce(var.rg.overriden_name, "${var.app.prefix}-${var.app.stage}")
-}
-
-resource "azurerm_resource_group" "main" {
-  name = local.resource_group_name
-  location = var.location
-  //https://developer.hashicorp.com/terraform/language/meta-arguments/lifecycle#prevent_destroy
-}
