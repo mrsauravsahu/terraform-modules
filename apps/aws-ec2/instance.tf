fb5531b681113457fed644ffdb5d58d45875765d
Example <mrsauravsahu@outlook.com>
2024-06-18 02:19:56 +0530

    chore: remove other files

--- apps/aws-ec2/instance.tf
+++ apps/aws-ec2/instance.tf
@@ -16,6 +16,6 @@ resource "aws_instance" "main" {
 
 resource "aws_key_pair" "main" {
   # TODO: add a hash at the end
-  key_name   = "instance-key-pair-${var.app.name}"
+  key_name   = "instance-key-pair"
   public_key = tls_private_key.main.public_key_openssh
 }
--- apps/az-fullstack-app-service/db.outputs.tf
+++
@@ -1,6 +0,0 @@
-output "db" {
-  value = {
-    password = azurerm_postgresql_flexible_server.main.administrator_password
-  }
-  sensitive = true
-}
